90d37465e7862aaa9628f7e4bef21c0cc1c0fac8
ewno92 <77653755+ewno92@users.noreply.github.com>
2021-11-24 15:36:29 -0800

    post detail page

--- components/home/HomeTable.js
+++ components/home/HomeTable.js
@@ -1,34 +1,41 @@
-import React, { useEffect } from 'react';
-import Link from 'next/link';
+import React, { useEffect } from "react";
+import Link from "next/link";
+import Router from "next/router";
 
 const HomeTable = ({ title, jobs }) => {
-	const jobsCopy = [...jobs];
+  const jobsCopy = [...jobs];
 
-	return (
-		<table id="home-table" className="table">
-			<thead>
-				<tr>
-					<Link href="/jobs">
-						<th className="d-flex justify-content-between header-container" scope="col">
-							<span className="title">{title}</span>
-							<span className="icon">+</span>
-						</th>
-					</Link>
-				</tr>
-			</thead>
-			<tbody>
-				{jobsCopy.splice(0, 3).map((job, index) => {
-					return (
-						<tr key={index}>
-							<td style={{ cursor: 'pointer', fontSize: '12px' }}>
-								<p>{job.title}</p>
-							</td>
-						</tr>
-					);
-				})}
-			</tbody>
-		</table>
-	);
+  return (
+    <table id="home-table" className="table">
+      <thead>
+        <tr>
+          <Link href="/jobs">
+            <th
+              className="d-flex justify-content-between header-container"
+              scope="col"
+            >
+              <span className="title">{title}</span>
+              <span className="icon">+</span>
+            </th>
+          </Link>
+        </tr>
+      </thead>
+      <tbody>
+        {jobsCopy.splice(0, 3).map((job, index) => {
+          return (
+            <tr key={index}>
+              <td
+                style={{ cursor: "pointer", fontSize: "12px" }}
+                onClick={() => Router.push(`/jobs/ca/post/${job.id}`)}
+              >
+                <p>{job.title}</p>
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
 };
 
 export default HomeTable;
--- pages/jobs/ca/post/[id].js
+++ pages/jobs/ca/post/[id].js
@@ -1,17 +1,49 @@
-import React from 'react';
-import Head from 'next/head';
-import axios from 'axios';
+import React from "react";
+import Head from "next/head";
+import axios from "axios";
+import { Container, Row, Col } from "react-bootstrap";
+
+import { BsEyeFill } from "react-icons/bs";
+//https://react-icons.github.io/react-icons/
 
 const Index = ({ post }) => {
-	console.log(post);
-	return <div>test</div>;
+  console.log(post);
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <h2 className="my-2">{post.title}</h2>
+        </Col>
+      </Row>
+
+      <Row className="bg-light border-top">
+        <Col className="d-flex justify-content-between py-2">
+          <span>{post.author}</span>
+          <span>{post.date}</span>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <BsEyeFill color="gray" />
+          <span className="px-1">{post.views}</span>
+        </Col>
+      </Row>
+      <Row className="pt-2">
+        <span>{post.category}</span>
+        <span>{post.location}</span>
+        <span>{post.phone}</span>
+      </Row>
+    </Container>
+  );
 };
 
 export const getServerSideProps = async (context) => {
-	const { id } = context.query;
-	console.log(id);
-	const post = await axios.get(`https://korean-community.herokuapp.com/api/ca/la/find-job/${id}`).then((res) => res.data);
-	return { props: { post } };
+  const { id } = context.query;
+  console.log(id);
+  const post = await axios
+    .get(`https://korean-community.herokuapp.com/api/ca/la/find-job/${id}`)
+    .then((res) => res.data);
+  return { props: { post } };
 };
 
 export default Index;
